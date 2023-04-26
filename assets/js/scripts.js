f32fce042f8b6507d8e011f7c7c2931b3f655e01
John Van Brocklin <johnvbrocklin@icloud.com>
2023-04-26 18:50:16 +0000

    final site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,234 +32,229 @@ $('.slider-for').slick({
   });
 
   async function GetData() {
-    "use strict";
     var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055680,07055646,07055780,07055660&period=P7D&parameterCd=00065&siteStatus=active"
     var msgObject = await fetch(myURL);
     var msgJSONText = await msgObject.text();
     var msg = JSON.parse(msgJSONText);
-
-
+    
     /* Site 1 */
     var dates = [];
     var values = [];
     /* fLen contains the length of the array (number of values) */
-    var fLen = msg.value.timeSeries[0].values[0].value.length
-    for (var i = 0; i < fLen; i++) {
+    fLen = msg.value.timeSeries[0].values[0].value.length
+    for (i = 0; i < fLen; i++) {
         values[i] = msg.value.timeSeries[0].values[0].value[i].value
         dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime
-    };
+    }
     var sitename = msg.value.timeSeries[0].sourceInfo.siteName
     var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value
     var siteDescription = msg.value.timeSeries[0].variable.variableDescription
-
-    /* Put your code here to display a graph of values and dates for Site 1*/
+    
     var ctx = document.getElementById("chartjs-0");
-
     var myChart = new Chart(ctx, {
-        "type": "line",
+        "type":"line",
         "data": {
             "labels": dates,
-            "datasets": [{
+            "datasets":[{
+            "label":"Height",
+            "data": values,
+            "fill":false,
+            "borderColor":"rgb(0, 0, 255)",
+            "lineTension":0.1
+        }]},
+            "options":{
+                responsive: false,
+                maintainAspectRatio: true,
+                title: {
+                    display: true,
+                    text: sitename + ' ' + sitecode,
+                    fontSize : 20
+                },
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                             displayFormats: {
+                                day: 'MMM D'
+                            }
+                        }
+                    }],
+    
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: siteDescription
+                        }
+                    }]
+    
+                }
+            }
+        }
+    );
+    
+    
+        /* Site 2 */
+        var dates = [];
+        var values = [];
+        /* fLen contains the length of the array (number of values) */
+        fLen = msg.value.timeSeries[1].values[0].value.length
+        for (i = 0; i < fLen; i++) {
+            values[i] = msg.value.timeSeries[1].values[0].value[i].value
+            dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
+        }
+        var sitename = msg.value.timeSeries[1].sourceInfo.siteName
+        var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
+        var siteDescription = msg.value.timeSeries[1].variable.variableDescription
+        
+        var ctx = document.getElementById("chartjs-1");
+        var myChart = new Chart(ctx, {
+            "type":"line",
+            "data": {
+                "labels": dates,
+                "datasets":[{
+                "label":"Height",
                 "data": values,
-                "label": sitename,
-                "fill": false,
-              family: "Arial"
-            }]
-        },
-        "options":{ 
-            responsive: false,
-            maintainAspectRatio: false,
-            title: {
-                display: true,
-                text: sitename,
-                fontSize : 18
-            },
-
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    time: {
-                            displayFormats: {
-                            day: 'MMM D'
+                "fill":false,
+                "borderColor":"rgb(115, 147, 179)",
+                "lineTension":0.1
+            }]},
+                "options":{
+                    responsive: false,
+                    maintainAspectRatio: true,
+                    title: {
+                        display: true,
+                        text: sitename + ' ' + sitecode,
+                        fontSize : 20
+                    },
+                    scales: {
+                        xAxes: [{
+                            type: 'time',
+                            time: {
+                                 displayFormats: {
+                                    day: 'MMM D'
+                                }
+                            }
+                        }],
+        
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: siteDescription
+                            }
+                        }]
+        
+                    }
+                }
+            }
+        );
+    
+            /* Site 3 */
+            var dates = [];
+            var values = [];
+            /* fLen contains the length of the array (number of values) */
+            fLen = msg.value.timeSeries[2].values[0].value.length
+            for (i = 0; i < fLen; i++) {
+                values[i] = msg.value.timeSeries[2].values[0].value[i].value
+                dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
+            }
+            var sitename = msg.value.timeSeries[2].sourceInfo.siteName
+            var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
+            var siteDescription = msg.value.timeSeries[2].variable.variableDescription
+            
+            var ctx = document.getElementById("chartjs-2");
+            var myChart = new Chart(ctx, {
+                "type":"line",
+                "data": {
+                    "labels": dates,
+                    "datasets":[{
+                    "label":"Height",
+                    "data": values,
+                    "fill":false,
+                    "borderColor":"rgb(8, 143, 143)",
+                    "lineTension":0.1
+                }]},
+                    "options":{
+                        responsive: false,
+                        maintainAspectRatio: true,
+                        title: {
+                            display: true,
+                            text: sitename + ' ' + sitecode,
+                            fontSize : 20
+                        },
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                                time: {
+                                     displayFormats: {
+                                        day: 'MMM D'
+                                    }
+                                }
+                            }],
+            
+                            yAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: siteDescription
+                                }
+                            }]
+            
                         }
                     }
-                }],
-                
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: siteDescription        
-    }
-    }]
-        }
-        }
-    });
-
-    /* Site 2 */
-    var dates = [];
-    var values = [];
-    /* fLen contains the length of the array (number of values) */
-    fLen = msg.value.timeSeries[1].values[0].value.length
-    for (i = 0; i < fLen; i++) {
-        values[i] = msg.value.timeSeries[1].values[0].value[i].value
-        dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
-    }
-    var sitename = msg.value.timeSeries[1].sourceInfo.siteName
-    var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
-    var siteDescription = msg.value.timeSeries[1].variable.variableDescription
-
-    /* Put your code here to display a graph of values and dates for Site 2*/
-    var ctx = document.getElementById("chartjs-1");
-
-    var myChart = new Chart(ctx, {
-        "type": "line",
-        "data": {
-            "labels": dates,
-            "datasets": [{
-                "data": values,
-                "label": sitename,
-                "fill": false,
-              family: "Arial"
-            }]
-        },
-        "options":{ 
-            responsive: false,
-            maintainAspectRatio: false,
-            title: {
-                display: true,
-                text: sitename,
-                fontSize : 18
-            },
-
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    time: {
-                            displayFormats: {
-                            day: 'MMM D'
+                }
+            );
+    
+            /* Site 4 */
+            var dates = [];
+            var values = [];
+            /* fLen contains the length of the array (number of values) */
+            fLen = msg.value.timeSeries[3].values[0].value.length
+            for (i = 0; i < fLen; i++) {
+                values[i] = msg.value.timeSeries[3].values[0].value[i].value
+                dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
+            }
+            var sitename = msg.value.timeSeries[3].sourceInfo.siteName
+            var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
+            var siteDescription = msg.value.timeSeries[3].variable.variableDescription
+            
+            var ctx = document.getElementById("chartjs-3");
+            var myChart = new Chart(ctx, {
+                "type":"line",
+                "data": {
+                    "labels": dates,
+                    "datasets":[{
+                    "label":"Height",
+                    "data": values,
+                    "fill":false,
+                    "borderColor":"rgb(0, 150, 255)",
+                    "lineTension":0.1
+                }]},
+                    "options":{
+                        responsive: false,
+                        maintainAspectRatio: true,
+                        title: {
+                            display: true,
+                            text: 'Buffalo River near Carver, AR ' + sitecode,
+                            fontSize : 20
+                        },
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                                time: {
+                                     displayFormats: {
+                                        day: 'MMM D'
+                                    }
+                                }
+                            }],
+            
+                            yAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: siteDescription
+                                }
+                            }]
+            
                         }
                     }
-                }],
-                
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: siteDescription        
-    }
-    }]
-        }
-        }
-    });
-
-    /* Site 3 */
-    var dates = [];
-    var values = [];
-    /* fLen contains the length of the array (number of values) */
-    fLen = msg.value.timeSeries[2].values[0].value.length
-    for (i = 0; i < fLen; i++) {
-        values[i] = msg.value.timeSeries[2].values[0].value[i].value
-        dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
-    }
-    var sitename = msg.value.timeSeries[2].sourceInfo.siteName
-    var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
-    var siteDescription = msg.value.timeSeries[2].variable.variableDescription
-
-    /* Put your code here to display a graph of values and dates for Site 3*/
-    var ctx = document.getElementById("chartjs-2");
-
-    var myChart = new Chart(ctx, {
-        "type": "line",
-        "data": {
-            "labels": dates,
-            "datasets": [{
-                "data": values,
-                "label": sitename,
-                "fill": false,
-              family: "Arial"
-            }]
-        },
-        "options":{ 
-            responsive: false,
-            maintainAspectRatio: false,
-            title: {
-                display: true,
-                text: sitename,
-                fontSize : 18
-            },
-
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    time: {
-                            displayFormats: {
-                            day: 'MMM D'
-                        }
-                    }
-                }],
-                
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: siteDescription        
-    }
-    }]
-        }
-        }
-    });
-
-    /* Site 2 */
-    var dates = [];
-    var values = [];
-    /* fLen contains the length of the array (number of values) */
-    fLen = msg.value.timeSeries[3].values[0].value.length
-    for (i = 0; i < fLen; i++) {
-        values[i] = msg.value.timeSeries[3].values[0].value[i].value
-        dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
-    }
-    var sitename = msg.value.timeSeries[3].sourceInfo.siteName
-    var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
-    var siteDescription = msg.value.timeSeries[3].variable.variableDescription
-
-    /* Put your code here to display a graph of values and dates for Site 4*/
-    var ctx = document.getElementById("chartjs-3");
-
-    var myChart = new Chart(ctx, {
-        "type": "line",
-        "data": {
-            "labels": dates,
-            "datasets": [{
-                "data": values,
-                "label": sitename,
-                "fill": false,
-              family: "Arial"
-            }]
-        },
-        "options":{ 
-            responsive: false,
-            maintainAspectRatio: false,
-            title: {
-                display: true,
-                text: sitename,
-                fontSize : 18
-            },
-
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    time: {
-                            displayFormats: {
-                            day: 'MMM D'
-                        }
-                    }
-                }],
-                
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: siteDescription        
-    }
-    }]
-        }
-        }
-    });
-}
+                }
+            );
+  }
